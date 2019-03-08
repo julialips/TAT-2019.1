ff1bfefc6bd598beba735b043c06513e12923a2b
Липская Юлия <yuliya.santotsckay@yandex.ru>
2019-03-08 19:45:22 +0300

    Добавьте файлы проекта.

---
+++ TAT-2019.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAT_2019._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
