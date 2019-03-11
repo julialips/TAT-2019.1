82bad2b46691cef93bc77f99c3dbd3f2d7603c7c
Липская Юлия <yuliya.santotsckay@yandex.ru>
2019-03-11 23:43:02 +0300

    added exeption and renaim main program

--- TAT-2019.1/DifferentSimbols.cs
+++ TAT-2019.1/DifferentSimbols.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TAT_2019._1
 {
---
+++ TAT-2019.1/EntryPoint.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TAT_2019._1
+{
+    /// <summary>
+    /// the main class Program,entry point to the application.
+    ///From here we get the string and check its length.
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args[0].Length < 2 && args.Length < 2)
+                {
+                    throw new FormatException();
+                }
+                if (args.Length != 0) // Checking the condition for the argument
+                {
+                    string strfromcommandline = null;
+                    foreach (string element in args)
+                    {
+                        strfromcommandline += element; // rewrite items from command line to variable
+                    }
+
+                    DifferentSimbols obj = new DifferentSimbols();
+                    obj.SearchDifferentSimbols(strfromcommandline);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("String must be longer than 1 simbol");
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Oops,something went wrong,try again!");
+            }
+
+            Console.Read();
+        }
+    }
+}
