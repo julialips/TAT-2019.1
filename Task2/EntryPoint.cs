680fe583dea5a5f942dbc86cfd5f030185bc36f4
Липская Юлия <yuliya.santotsckay@yandex.ru>
2019-03-16 11:50:52 +0300

    many changes

--- Task2/EntryPoint.cs
+++ Task2/EntryPoint.cs
@@ -13,25 +13,41 @@ namespace Task2
         /// entrance to the application
         /// </summary>
         /// <param name="args"></param>
-        static void Main(string[] args)
+        /// <returns 0>Good start of program</returns>
+        /// <returns 1>Empty string</returns> 
+        /// <returns 2>Something went wrong</returns>
+        /// 
+        static int Main(string[] args)
         {
             try
             {
+                if(args.Length == 0)
+                    throw new FormatException();
+
                 foreach (string line in args)
                 {
                     Console.Write(line);
                     Console.Write("-->");
-                    // line += args;
-                    var RushianRules = new RushianRules();
-                    RushianRules.ConvertToPhonemes(args);
-                    Console.Read();
-
                 }
-
+                RushianRules[] converterToPhonemes = new RushianRules[args.Length];
+                for (int i = 0; i < converterToPhonemes.Length; i++)
+                {
+                    converterToPhonemes[i] = new RushianRules(args[i]);
+                    converterToPhonemes[i].PrintPhonemes(converterToPhonemes[i].ConvertToPhonemes());
+                }
+                Console.Read();
+                return 0;
             }
+            catch (FormatException)
+            {
+                Console.WriteLine("The entered string is empty, try again!");
+                return 1;
+            }
+
             catch (Exception)
             {
                 Console.WriteLine("Oops,something went wrong,try again!");
+                return 2;
             }
           
             //var RushianRules = new RushianRules();
--- Task2/RushianRules.cs
+++ Task2/RushianRules.cs
@@ -1,37 +1,152 @@
 ﻿using System;
 using System.Text;
-
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Task2
 {
     class RushianRules
     {
-       public void ConvertToPhonemes(string[] args)
-        {
-         for(int i = 0; i < args.Length; i++)
-            {
-                args[i] = null;
-                switch (args[i])
-                {
-                    case "м" :
-                        string newstr = args[i];
-                        meth1(newstr);
-                        break;
+       
+        private int accent = -2;
+        private string simbol = string.Empty;
+        private StringBuilder phonemes = new StringBuilder();
 
-                    default:
-                        Console.WriteLine("Default case");
-                        break;
+
+        private readonly List<char> vowelsSoft = new List<char>()
+            {
+                'и', 'е', 'ё', 'ю', 'я'
+            };
+
+        private readonly List<char> vowelsHard = new List<char>()
+            {
+                'а', 'о', 'э', 'ы', 'у'
+            };
+
+        private readonly List<char> consonantsHard = new List<char>()
+            {
+               'б', 'в', 'г', 'д', 'ж', 'з','к', 'л', 'м', 'н',
+               'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ш'
+            };
+
+        private readonly List<char> consonantsSoft = new List<char>()
+            {
+                'ц', 'щ', 'й'
+            };
+
+        private readonly List<char> consonantsDeaf = new List<char>()
+            {
+               'к', 'п', 'с', 'т', 'ф', 'х', 'ц', 'ш', 'щ'
+            };
+
+        private readonly List<char> consonantsVoiced = new List<char>()
+            {
+               'б', 'в', 'г', 'д', 'ж', 'з', 'л', 'м', 'н',
+                'р', 'й'
+            };
+
+        private readonly Dictionary <char, char> vowelBeforeConsonant = new Dictionary<char, char>
+        {
+            ['ю'] = 'у',
+            ['я'] = 'а',
+            ['ё'] = 'о',
+            ['е'] = 'э'
+        };
+        private readonly Dictionary <char, string> vowelBeforeVowel = new Dictionary<char, string>
+        {
+            ['ю'] = "йу",
+            ['я'] = "йа",
+            ['ё'] = "йо",
+            ['е'] = "йэ"
+        };
+        private readonly Dictionary <char, char> RingingDeaf = new Dictionary<char, char>
+        {
+            ['б'] = 'п',
+            ['в'] = 'ф',
+            ['г'] = 'к',
+            ['д'] = 'т',
+            ['ж'] = 'ш',
+            ['з'] = 'с'
+        };
+
+    
+
+        public RushianRules(string line)
+        {
+            simbol = line;
+        }
+
+        public StringBuilder RushianRules()
+        {
+            if (simbol.Contains('+'))
+            {
+                accent = simbol.IndexOf('+') - 1;
+                simbol = simbol.Remove(simbol.IndexOf('+'), 1);
+            }
+
+            for (int i = 0; i < simbol.Length; i++)
+            {
+                switch (DefineTypeOfSymbol(simbol[i], vowels, consonants))
+                {
+                    case "vowel":
+
+                        if (i != 0 && DefineTypeOfSimbol(simbol[i - 1], vowels, consonants) ==
+                            "consonant" && vowelBeforeConsonant.ContainsKey(simbol[i]))
+                        {
+                            // Add value to phonemes.
+                            phonemes.Append("'" + vowelBeforeConsonant[simbol[i]]);
+                            continue;
+
+                        }
+                        else if (vowelBeforeVowel.ContainsKey(simbol[i]))
+                        {
+                            phonemes.Append(vowelBeforeVowel[simbol[i]]);
+                            continue;
+                        }
+
+                        // Go if dictionaries don't contains this vowel.
+                      
+                        continue;
+
+                    case "different":
+                        // If  'ъ', continue.
+                        if (simbol[i] == 'ь')
+                        {
+                            phonemes.Append("'");
+                        }
+                        continue;
                 }
             }
-             void meth1(string newstr)
+            return phonemes;
+        }
+
+        public string DefineTypeOfSymbol(char simbol, List<char> vowels, List<char> consonants)
+        {
+            if (vowels.Contains(simbol))
             {
-                Console.WriteLine(newstr);
+                return "vowel";
             }
-        
+            else if (consonants.Contains(simbol))
+            {
+                return "consonant";
+            }
+            return "different";
+        }
+
+        public void AddOtherLetter(int index, char simbol, int accent, ref StringBuilder phonemes)
+        {
+            if (simbol == 'о' && index != accent)
+            {
+                phonemes.Append("а");
+                return;
+            }
+            phonemes.Append(simbol);
+        }
 
 
-
-
+        public void PrintPhonemes(StringBuilder phonemes)
+        {
+            Console.WriteLine(simbol + " -> " + phonemes);
         }
 
     }
