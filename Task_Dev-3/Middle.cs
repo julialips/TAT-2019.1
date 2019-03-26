f9d52241ee6d2538d2d2730a507a561e2fdeef10
Julia_Lipskaya <45575431+julialips@users.noreply.github.com>
2019-03-26 16:51:26 +0300

    Update Middle.cs

--- Task_Dev-3/Middle.cs
+++ Task_Dev-3/Middle.cs
@@ -1,10 +1,13 @@
-﻿namespace Task_Dev_3
+namespace Task_Dev_3
 {
     /// <summary>
     /// child class from Junior class, to definition variables
     /// </summary>
     class Middle : Junior
     {
+        /// <summary>
+        /// it's a constructor,where we redefining fields
+        /// </summary>
         public Middle()
         {
             Salary += 250; //500
