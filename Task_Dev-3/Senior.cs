7d91ee6df846b8c1cf0272741e46bdf996312bba
Julia_Lipskaya <45575431+julialips@users.noreply.github.com>
2019-03-26 16:50:32 +0300

    Update Senior.cs

--- Task_Dev-3/Senior.cs
+++ Task_Dev-3/Senior.cs
@@ -1,10 +1,13 @@
-﻿namespace Task_Dev_3
+namespace Task_Dev_3
 {
     /// <summary>
     /// child class from Middle class, to definition variables
     /// </summary>
     class Senior : Middle
-    {
+    {   
+        /// <summary>
+        /// it's a constructor,where we redefining fields
+        /// </summary>
         public Senior()
         {
             Salary += 450; //950
