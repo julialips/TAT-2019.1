5782fe891fb677c9479a4c2f5cbee35cdeac3b1e
Julia_Lipskaya <45575431+julialips@users.noreply.github.com>
2019-03-26 16:52:35 +0300

    Update Junior.cs

--- Task_Dev-3/Junior.cs
+++ Task_Dev-3/Junior.cs
@@ -1,10 +1,13 @@
-﻿namespace Task_Dev_3
+namespace Task_Dev_3
 {
     /// <summary>
     /// child class from Worker class, to definition variables
     /// </summary>
     class Junior : Worker
     {
+        /// <summary>
+        /// it's a constructor,where we redefining fields
+        /// </summary>
         public Junior()
         {
             Salary *= 1; //250
