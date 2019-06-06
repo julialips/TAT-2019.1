e56f64d7180b3abe97baff3f85ebafc64c13db53
Липская Юлия <yuliya.santotsckay@yandex.ru>
2019-06-06 12:01:08 +0300

    changes

--- Task2/DownloadingKurs.cs
+++ Task2/DownloadingKurs.cs
@@ -17,8 +17,8 @@ namespace valuta
         public IWebElement SellUSD => this.driver.FindElement(By.XPath("//*[@class=['BlockCurrencyExchangeRates__rate - item_sell - rate'][1]"));
         public IWebElement BuyEUR => this.driver.FindElement(By.XPath("//*[@class='BlockCurrencyExchangeRates__rate - item_buy - rate'[2]"));
         public IWebElement SellEUR => this.driver.FindElement(By.XPath("//*[@class=['BlockCurrencyExchangeRates__rate - item_sell - rate'][2]"));
-        public IWebElement BuyRus => this.driver.FindElement(By.XPath("//*[@class='BlockCurrencyExchangeRates__rate - item_buy - rate'[3]"));
-        public IWebElement SellRus => this.driver.FindElement(By.XPath("//*[@class=['BlockCurrencyExchangeRates__rate - item_sell - rate'][3]"));
+        public IWebElement BuyRub => this.driver.FindElement(By.XPath("//*[@class='BlockCurrencyExchangeRates__rate - item_buy - rate'[3]"));
+        public IWebElement SellRub => this.driver.FindElement(By.XPath("//*[@class=['BlockCurrencyExchangeRates__rate - item_sell - rate'][3]"));
 
         public DownloaingKurs(IWebDriver driver)
         {
@@ -33,8 +33,24 @@ namespace valuta
             currency.Add(new Currency("BuyUSD", BuyUSD));
 
             new WebDriverWait(this.driver, TimeSpan.FromSeconds(10)).Until(x => this.SellUSD.Enabled);
-            string SellUSD = this.BuyUSD.Text;
+            string SellUSD = this.SellUSD.Text;
             currency.Add(new Currency("SellUSD", SellUSD));
+
+            new WebDriverWait(this.driver, TimeSpan.FromSeconds(10)).Until(x => this.BuyUSD.Enabled);
+            string BuyEUR = this.BuyEUR.Text;
+            currency.Add(new Currency("BuyEUR", BuyEUR));
+
+            new WebDriverWait(this.driver, TimeSpan.FromSeconds(10)).Until(x => this.SellUSD.Enabled);
+            string SellEUR = this.SellEUR.Text;
+            currency.Add(new Currency("SellEUR", SellEUR));
+
+            new WebDriverWait(this.driver, TimeSpan.FromSeconds(10)).Until(x => this.BuyUSD.Enabled);
+            string BuyRub = this.BuyRub.Text;
+            currency.Add(new Currency("BuyRub", BuyRub));
+
+            new WebDriverWait(this.driver, TimeSpan.FromSeconds(10)).Until(x => this.SellUSD.Enabled);
+            string SellRub = this.SellRub.Text;
+            currency.Add(new Currency("SellRub", SellRub));
         }
     }
 }
